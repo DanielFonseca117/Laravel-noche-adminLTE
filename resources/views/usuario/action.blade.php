8e6d7532f3e29edeb2bc8c86de1e54e3626be22e
DanielFonseca117 <danifon44@email.com>
2025-09-23 18:53:39 -0500

    actualizado

---
+++ resources/views/usuario/action.blade.php
@@ -0,0 +1,40 @@
+@extends('plantilla.app') 
+@section('contenido')       
+        <div class="app-content">
+          <!--begin::Container-->
+          <div class="container-fluid">
+            <!--begin::Row-->
+            <div class="row">
+              <div class="col-md-12">
+                <div class="card mb-4">
+                  <div class="card-header"><h3 class="card-title">Usuarios</h3></div>
+                  <!-- /.card-header -->
+                  <div class="card-body">
+
+                    <div>
+                        <form action="" method="post" id="formRegistroUsuario">
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                  </div>
+                  <!-- /.card-body -->
+     
+            
+                <!-- /.card -->
+
+                <!-- /.card -->
+              </div>
+              <!-- /.col -->
+              <!-- /.col -->
+            </div>
+            <!--end::Row-->
+          </div>
+          <!--end::Container-->
+        </div>
+
+@endsection
